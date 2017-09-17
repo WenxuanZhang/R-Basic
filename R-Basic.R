a525c332388e57a8e03cd40278103673535841b2
Wenxuan Zhang <ll.wxzhang@gmail.com>
2017-09-17 00:01:13 -0400

    first commit

---
+++ R-Basic.R
@@ -0,0 +1,17 @@
+########################################################################################################################
+# R Basic 
+# 
+# Description: this doc intended to include the most frequently & Basic used function in R
+#
+#
+#
+########################################################################################################################
+# 0.Global
+# 0.1 Update R
+if(!require(installr)) { install.packages("installr"); require(installr) }
+updateR()
+# 1. Data Manipulatioin 
+# 1.1 Create
+# 1.2 Acess & Modify
+
+# 1.3 
